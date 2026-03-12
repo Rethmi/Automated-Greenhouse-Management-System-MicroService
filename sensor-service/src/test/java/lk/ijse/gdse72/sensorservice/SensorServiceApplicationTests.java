d909abbd76ae0e2713657deac55e23d851f3722b
Rethmi Thennakoon <reththennakoon.edu@gmail.com>
2026-03-12 11:03:42 +0530

    Setup sensor-service

---
+++ sensor-service/src/test/java/lk/ijse/gdse72/sensorservice/SensorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse72.sensorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
